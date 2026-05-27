05afeb8fe5ad93f35aaf74f8ff3dab9de12a4e95
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-27 17:11:48 +0530

    fix: useOnlineStatus by using abortcontroller

--- src/hooks/useOnlineStatus.ts
+++ src/hooks/useOnlineStatus.ts
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useOnlineStatus() {
     const [online, setOnline] = useState(false);
-    window.addEventListener("offline", () => {
-        setOnline(false);
-    });
-    window.addEventListener("online", () => {
-        setOnline(true);
-    });
+    useEffect(() => {
+        const abortController = new AbortController();
+        window.addEventListener(
+            "offline",
+            () => {
+                setOnline(false);
+            },
+            { signal: abortController.signal },
+        );
+        window.addEventListener(
+            "online",
+            () => {
+                setOnline(true);
+            },
+            { signal: abortController.signal },
+        );
+        return () => {
+            abortController.abort();
+        };
+    }, []);
     return online;
 }
 
