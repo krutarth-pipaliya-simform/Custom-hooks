2108171921c6d5b651009f73b8db57e04d52a8c5
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-27 15:55:16 +0530

    add useCounter

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function useCounter(initialValue = 0) {
+    const [count, setCount] = useState(initialValue);
+    const increment = () => {
+        setCount((prev) => prev + 1);
+    };
+    const decrement = () => {
+        setCount((prev) => prev - 1);
+    };
+    const reset = () => {
+        setCount(initialValue);
+    }
+    return {
+        count, setCount, increment, decrement, reset
+    }
+}
+
+function Counter() {
+    const { count, increment, decrement, reset } = useCounter(5);
+}
